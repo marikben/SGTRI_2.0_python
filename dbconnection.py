df6eea50e3ad352aae58ffffb6ef653105b7e40e
marikben <70907667+marikben@users.noreply.github.com>
2022-03-06 18:22:08 +0200

    testing server connection

--- dbconnection.py
+++ dbconnection.py
@@ -17,13 +17,13 @@ api = Api(app)
 class HESSO(Resource):
     @app.route("/cocktails/<string:name>", methods=["GET"])
     def get(name):
-        project = mongo.db.hesso.find_one_or_404({"name": name})
+        project = mongo.db.cocktails.find_one_or_404({"name": name})
         return Project(**project).to_json() 
 
     def post(self):   
         raw_project = request.get_json()
         project = Project(**raw_project)
-        insert_result = mongo.db.hesso.insert_one(project.to_bson())
+        insert_result = mongo.db.cocktails.insert_one(project.to_bson())
         project.id = PydanticObjectId(str(insert_result.inserted_id))
         print(project)
         return project.to_json()
